49b013e596e0bdad90ac7c067c65ef2ae970b937
Axel S. Toro Vega <axelsariel@gmail.com>
2024-02-13 23:15:24 -0500

    Added routes in

--- README.md
+++ README.md
@@ -1,9 +1,50 @@
-# diy-ml
+## diy-ml
 DIY ML API Service for EC530
 
 DIY ML is an API to train, test, and deploy ML models.
 
-# Setup
+# User Stories
+
+Below is a list of User Stories for reference, from the class slides
+
+- API user should be able to create a ML image classification or Object detection project for Training and Inference
+- API user should be able to upload data (images) for training in a project
+- API user should be able to upload label or class data for images in a project
+- API user should be able to analyze data before training
+- API user should be able to add or remove training points
+- API user should be able to configure training parameters
+- API user should be able when the training is completed to get training stats
+- API user should be able to test a model using new dataset and get results
+- API user should be able to deploy a model to be used for inference and should be able to get a unique API to use for a project-iteration combination
+- API user should be able to run and track iterations of training
+- API user should be able to use inference API to run and get results on an image
+- ALL APIs should be independent of the ML model and data
+- A project is associated with a user
+
+# Routes
+Endpoint                 Methods  Rule                                          
+-----------------------  -------  ----------------------------------------------
+datasets_create          POST     /diyml/datasets/create                        
+datasets_objects_create  POST     /diyml/datasets/objects/create                
+datasets_objects_delete  POST     /diyml/datasets/objects/delete                
+index                    GET      /                                             
+inference_delete         POST     /diyml/inference/delete                       
+inference_deploy         POST     /diyml/inference/deploy                       
+inference_infer          POST     /diyml/inference/<inferenceDeploymentId>/infer
+preprocess               POST     /diyml/preprocess                             
+projects_create          POST     /diyml/projects/create                        
+projects_delete          POST     /diyml/projects/delete                        
+test_create              POST     /diyml/test/start                             
+test_results             GET      /diyml/test/results                           
+test_status              POST     /diyml/test/status                            
+test_stop                POST     /diyml/test/stop                              
+training_results         GET      /diyml/training/results                       
+training_start           POST     /diyml/training/start                         
+training_status          GET      /diyml/training/status                        
+training_stop            POST     /diyml/training/stop                          
+users_create             POST     /diyml/users/create  
+
+## Setup
 
 1. Clone repo
 ```
--- server.py
+++ server.py
@@ -1,10 +1,187 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return jsonify({
+        'response' : 'Hello world!'
+    })
+
+# Users
+@app.route('/diyml/users/create', methods=['POST'])
+def users_create():
+    input = request.get_json
+    username = input['username']
+    return jsonify({
+        'userId' : 'userId_here'
+    })
+
+# Projects
+@app.route('/diyml/projects/create', methods=['POST'])
+def projects_create():
+    input = request.get_json
+    userId = input['userId']
+    projectType = input['projectType']
+    projectName = input['projectName']
+    return jsonify({
+        'projectId' : 'uuid',
+        'projectType' : 'project_type',
+        'projectName' : 'project_name'
+    })
+
+@app.route('/diyml/projects/delete', methods=['POST'])
+def projects_delete():
+    input = request.get_json
+    projectId = input['projectId']
+    return jsonify({
+        'projectId' : 'uuid'
+    })
+
+# Datasets
+@app.route('/diyml/datasets/create', methods=['POST'])
+def datasets_create():
+    input = request.get_json
+    projectId = input['projectId']
+    datasetType = input['datasetType']
+    datasetName = input['datasetName']
+    datasetData = input['datasetData']
+    datasetMetadata = input['datasetMetadata']
+    return jsonify({
+        'datasetId' : 'uuid',
+        'projectId' : 'uuid',
+        'datasetType' : 'dataset_type',
+        'datasetName' : 'dataset_name'
+    })
+
+@app.route('/diyml/datasets/objects/create', methods=['POST'])
+def datasets_objects_create():
+    input = request.get_json
+    datasetId = input['datasetId']
+    objectData = input['objectData']
+    objectMetadata = input['objectMetadata']
+    return jsonify({
+        'datasetId' : 'uuid',
+        'objectId' : 'object_id'
+    })
+
+@app.route('/diyml/datasets/objects/delete', methods=['POST'])
+def datasets_objects_delete():
+    input = request.get_json
+    datasetId = input['datasetId']
+    objectId = input['objectId']
+    return jsonify({
+        'datasetId' : 'uuid',
+        'objectId' : 'object_id'
+    })
+
+# PreProcessing
+@app.route('/diyml/preprocess', methods=['POST'])
+def preprocess():
+    input = request.get_json
+    datasetId = input['datasetId']
+    return jsonify({
+        'datasetId' : 'uuid'
+    })
+
+# Training
+@app.route('/diyml/training/start', methods=['POST'])
+def training_start():
+    input = request.get_json
+    datasetId = input['datasetId']
+    trainingParameters = input['trainingParameters']
+    return jsonify({
+        'trainingJobId' : 'uuid'
+    })
+
+@app.route('/diyml/training/status', methods=['GET'])
+def training_status():
+    input = request.get_json
+    trainingJobId = input['trainingJobId']
+    return jsonify({
+        'trainingJobId' : 'uuid',
+        'trainingJobStatus' : 'status_here'
+    })
+
+@app.route('/diyml/training/stop', methods=['POST'])
+def training_stop():
+    input = request.get_json
+    trainingJobId = input['trainingJobId']
+    return jsonify({
+        'trainingJobId' : 'uuid'
+    })
+
+@app.route('/diyml/training/results', methods=['GET'])
+def training_results():
+    input = request.get_json
+    trainingJobId = input['trainingJobId']
+    return jsonify({
+        'trainingJobId' : 'uuid',
+        'trainingJobStats' : 'stats_here',
+        'trainedModelId' : 'model_id'
+    })
+
+# Test
+@app.route('/diyml/test/start', methods=['POST'])
+def test_create():
+    input = request.get_json
+    trainedModelid = input['trainedModelid']
+    testDatasetId = input['testDatasetId']
+    return jsonify({
+        'testJobId' : 'uuid'
+    })
+
+@app.route('/diyml/test/status', methods=['POST'])
+def test_status():
+    input = request.get_json
+    testJobId = input['testJobId']
+    return jsonify({
+        'testJobId' : 'uuid',
+        'testJobStatus' : 'status_here'
+    })
+
+@app.route('/diyml/test/stop', methods=['POST'])
+def test_stop():
+    input = request.get_json
+    testJobId = input['testJobId']
+    return jsonify({
+        'testJobId' : 'uuid'
+    })
+
+@app.route('/diyml/test/results', methods=['GET'])
+def test_results():
+    input = request.get_json
+    testJobId = input['testJobId']
+    return jsonify({
+        'testJobId' : 'uuid',
+        'testJobStats' : 'stats_here'
+    })
+
+# Inference
+@app.route('/diyml/inference/deploy', methods=['POST'])
+def inference_deploy():
+    input = request.get_json
+    trainedModelId = input['trainedModelId']
+    return jsonify({
+        'inferenceDeploymentId' : 'inferenceDeploymentId_here'
+    })
+
+@app.route('/diyml/inference/delete', methods=['POST'])
+def inference_delete():
+    input = request.get_json
+    inferenceDeploymentId = input['inferenceDeploymentId']
+    return jsonify({
+        'inferenceDeploymentId' : 'inferenceDeploymentId_here'
+    })
+
+@app.route('/diyml/inference/<inferenceDeploymentId>/infer', methods=['POST'])
+def inference_infer(inferenceDeploymentId):
+    input = request.get_json
+    inferenceDeploymentId = input['inferenceDeploymentId']
+    inferenceData = input['inferenceData']
+    return jsonify({
+        'inferenceResults' : 'inferenceResults_here'
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
