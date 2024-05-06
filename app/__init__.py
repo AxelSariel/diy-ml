aa75c42c2d631e2d9b692dcd841e37fa0d7882f1
Axel S. Toro Vega <axelsariel@gmail.com>
2024-05-05 21:21:38 -0400

    Fixed adding blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -15,4 +15,7 @@ def register_blueprints(app):
     from app.routes.users import bp as users_bp
     app.register_blueprint(users_bp)
 
+    from app.routes.datasets import bp as datasets_bp
+    app.register_blueprint(datasets_bp)
+
 app = create_app()
